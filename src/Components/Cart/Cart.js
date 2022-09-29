2bfcc4fabf8f81c1f900cf82dcb20e93c6124d66
MacBook <macbook@MacBooks-Air.Dlink>
2022-09-29 23:31:08 +0600

    commited

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -8,17 +8,37 @@ const Cart = ({ cart }) => {
     }
     return (
         <div>
-            <h3>CSE</h3>
+            <h3>Rafiqul Ala Mahid</h3>
+            <h4>Developer</h4>
+            <p>Favourite Games: </p>
+            <p>Cricket, Football, Chess, Tennis</p>
+            <h3>Add some break</h3>
+            <div className="cart-display">
+                <button>
+                    <p><span>10</span>s</p>
+                </button>
+                <button>
+                    <p><span>20</span>s</p>
+                </button>
+                <button>
+                    <p><span>30</span>s</p>
+                </button>
+                <button>
+                    <p><span>40</span>s</p>
+                </button>
+            </div>
                 <div>
-                    <h3>Exercise Details</h3>
+                    <h3>Games Cost Details</h3>
                     <div className='cart-display'>
-                    <h4>Exercise time</h4>
+                    <h4>Game time:</h4>
                     <p>{total}s</p>
                     </div>
-                    <div>
-                        <h4>Break time</h4>
+                    <div className='cart-display'>
+                    <h4>Break time:</h4>
+                    <p><span>0</span>s</p>
                     </div>
-                </div>
+            </div>
+            <button id='activity'>Activity Completed</button>
         </div>
     );
 };
