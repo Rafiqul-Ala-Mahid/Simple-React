6f2882a5786d76f0427a626cc628f710b8158045
MacBook <macbook@MacBooks-Air.Dlink>
2022-09-29 22:15:29 +0600

    fifth commit

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -10,22 +10,36 @@ const Exercise = () => {
             .then(res => res.json())
             .then(data => setGames(data))
         
-    },[])
+    }, [])
+    const handleAddToList = (games) => {
+        console.log(games)
+    }
     return (
         <div className='container'>
             <div className='game-list'>
-                <h1>Rafiqul Ala Mahid {games.length}</h1>
+                <h1>Select Favourite Games</h1>
+                <h3>Top most popular games {games.length}</h3>
                 <div className="game-cart">
                     {
                     games.map(game => <Game
                         key={game.id}
                         game={game}
+                        handleAddToList={handleAddToList}
                     ></Game>)
                 }
                 </div>
             </div>
             <div className='gameBreak'>
                 <h3>CSE</h3>
+                <div>
+                    <h3>Exercise Details</h3>
+                    <div>
+                        <h4>Exercise time</h4>
+                    </div>
+                    <div>
+                        <h4>Break time</h4>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Game.css'
 const Game = (props) => {
-    const {name,img,details,age,time}=props.game
+    const { handleAddToList } = props.game
+    const { name, img, details, age, time } = props.game
     return (
         <div className='element'>
             <img src={img} alt="" />
             <h3>{name}</h3>
             <p>{details}</p>
             <h5>For Age: {age}</h5>
-            <h5>Time required: {time}</h5>
-            <button className='btn'>
+            <h5>Time required: {time}s</h5>
+            <button onClick={()=>props.handleAddToList(props.game)} className='btn'>
                 <p>Add to list</p>
             </button>
         </div>
